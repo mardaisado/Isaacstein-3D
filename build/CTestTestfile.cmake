51aff88ac43742df96432b431e82eb0e73cdae4e
Aurelien Heritier <mardaisado@gamil.com>
2021-02-01 11:01:48 +0100

    add working sfml hello world

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/github/Isaacstein-3D
+# Build directory: C:/github/Isaacstein-3D/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
