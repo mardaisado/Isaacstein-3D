f0e49ead928fad7c30614caeaf39b4c6e60dc28c
mardaisado <50163923+mardaisado@users.noreply.github.com>
2021-02-01 11:23:28 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/github/Isaacstein-3D
-# Build directory: C:/github/Isaacstein-3D/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
